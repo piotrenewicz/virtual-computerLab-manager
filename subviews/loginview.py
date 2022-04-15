19cbeee65382a7289e73ba98a87e2e60034e4d75
Piotrenewicz <piotrekm9@onet.eu>
2022-04-15 21:40:16 +0200

    Templates for the login system

---
+++ subviews/loginview.py
@@ -0,0 +1,21 @@
+import utils as u
+from flask import Blueprint, render_template, request, redirect, session
+
+
+login_app = Blueprint('login', __name__, template_folder='templates')
+
+
+@login_app.route('/login', methods=('GET', 'POST'))
+def login():
+    if request.method == 'POST':
+        if u.perform_login():
+            return redirect(session.pop('login/return', None) or request.root_url)
+
+    return render_template('login.html')
+
+
+@login_app.route('/login/logout', methods=('GET', 'POST'))
+def logout():
+    session.pop('login', None)
+    session.pop('permLevel', None)
+    return redirect(request.referrer)
