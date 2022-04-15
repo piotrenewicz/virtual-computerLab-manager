99bd4f42c14026936739131b018a622831615fc8
Piotrenewicz <piotrekm9@onet.eu>
2022-04-15 21:45:04 +0200

    Decomposed main into subviews, And finished config template (TODO proxmox calls)

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import utils as u
-from flask import Flask, render_template, request, url_for, flash, redirect, session
-from werkzeug.exceptions import abort
-
+from flask import Flask, render_template, request, url_for, redirect, session
+from subviews.configview import config_app
+from subviews.loginview import login_app
 
 app = Flask(__name__)
-configured = u.check_configured()
+u.configured = u.check_configured()
 app.config['SECRET_KEY'] = u.get_config_value('SECRET_KEY') or "DEFAULT SECRET KEY. NOT VERY SECRET"
+
+app.register_blueprint(config_app)
+app.register_blueprint(login_app)
 
 
 @app.context_processor
@@ -20,110 +23,35 @@ def inject_default_context():
     )
 
 @app.before_request
-def check_method():
-    if request.method not in ('POST', 'GET'):
-        request.method = 'POST' if request.method.endswith('POST') else 'GET'
+def verify_session():
+    if not u.configured or \
+        request.path.startswith((url_for('login.login'), url_for('static', filename=''))):
+        return None
+
+    if not session.get('login'):
+        session['login/return'] = request.url
+        return redirect(url_for('login.login'))
+
+
+    # print(request.referrer)
+
+    # rules here:
+    # login screen gets a pass
+    # user screens get a required session.
+    # config screens require the session to be also level 4
+    #
+
+    # if session['authStatus']
 
 
 @app.route('/')
 def index():
-    if not configured:
-        return redirect(url_for('configuration'))
-    else:
-        return redirect(url_for('login'))
-    # so this will be a redirector, it will usually throw people into /login
-    # if we don't have a database it will create one and throw into setup.
+    if not u.configured:
+        return redirect(url_for('config.configuration'))
 
+    # overview dashboard idk, app
 
-@app.route('/config', methods=('GET', 'POST'))
-def configuration():
-    if configured:
-        #require login, to protect the settings from tampering
-        # we can't login before configured bc ldap is not set.
-        # deleagate this check to app.before_request
-        pass
-
-    # print(request.form)
-    with u.db_session() as cursor:
-        config_sections = [u.get_config_section(section=section_number, cursor=cursor) for section_number in range(6)]
-        config_sections[4]['all_users'] = u.get_users(None, cursor=cursor)
-        config_sections[4]['teachers'] = u.get_users(2, cursor=cursor)
-        config_sections[4]['maintainers'] = u.get_users(3, cursor=cursor)
-        config_sections[4]['admins'] = u.get_users(4, cursor=cursor)
-    config_sections[5]['configured'] = configured
-
-    return render_template('config.html', config_sections=config_sections, active_section=u.active_section)
-
-
-@app.route('/config/check_connection/<int:target>', methods=('POST',))
-def check_connection(target):
-    session['config/active_section'] = target
-    session['check/'+str(target)] = u.validate_connection_params(target)
-    return redirect(request.referrer, code=307)
-
-
-@app.route('/config/save/<int:target>', methods=('POST',))
-def save(target):
-    if u.validate_connection_params(target):
-        data = [(option, target, value) for option, value in u.form_reader(target).items()]
-        with u.db_session() as cursor:
-            cursor.executemany("replace into config(option, section, value) VALUES (?, ?, ?)", data)
-        flash("Settings saved!", 'success')
-        session['config/active_section'] = target + 1
-    else:
-        session['check/'+str(target)] = False
-        session['config/active_section'] = target
-    return redirect(request.referrer, code=307)
-
-@app.route('/config/sync/<int:target>', methods=('POST',))
-def sync_database(target):
-    session['config/active_section'] = 3
-    if target == 1:
-        # proxmox_sync()
-        pass
-    elif target == 2:
-        u.ldap_sync()
-
-    return redirect(request.referrer, code=307)
-
-@app.route('/config/set_permission', methods=('POST',))
-def set_permission():
-    session['config/active_section'] = 4
-    data = u.form_reader(4)
-    print(data)
-    with u.db_session() as cursror:
-        if session['config/setPermType'] == 2:
-            cursror.execute('select userID from user_table where fullname = ?', (data['userID'],))
-            data['userID'] = u.one_row_fix(cursror.fetchone())
-
-        if data['perm'] == 0:
-            cursror.execute('select count(groupID) from group_content where userID = ?', (data['userID'],))
-            if u.one_row_fix(cursror.fetchone()) > 0:
-                data['perm'] = 1
-
-        cursror.execute('update user_table set userPermission = ? where userID = ?', (data['perm'], data['userID']))
-
-    if data['perm']:
-        # TODO call proxmox to enable account data['userID']
-        pass
-    else:
-        # TODO call proxmox to disable account data['userID']
-        pass
-
-    return redirect(request.referrer, code=307)
-
-@app.route('/config/set_permission/<int:handle_type>', methods=('POST',))
-def set_permission_by(handle_type):
-    session['config/active_section'] = 4
-    session['config/setPermType'] = handle_type
-    return redirect(request.referrer, code=307)
-
-
-@app.route('/login')
-def login():
-    return render_template('login.html')
-
-
+    return render_template("base.html")
 
 
 # when shipping remember to put a block here, we don't want to allow production use of development mode
---
+++ subviews/configview.py
@@ -0,0 +1,106 @@
+import utils as u
+from flask import Blueprint, render_template, request, flash, redirect, session
+config_app = Blueprint('config', __name__, template_folder='templates')
+
+
+@config_app.route('/config', methods=('GET', 'POST'))
+def configuration():
+    if u.configured:
+        #require login, to protect the settings from tampering
+        # we can't login before configured bc ldap is not set.
+        # deleagate this check to config_page.before_request
+        pass
+
+    # print(request.form)
+    with u.db_session() as cursor:
+        config_sections = [u.get_config_section(section=section_number, cursor=cursor) for section_number in range(6)]
+        config_sections[4]['all_users'] = u.get_users(None, cursor=cursor)
+        config_sections[4]['teachers'] = u.get_users(2, cursor=cursor)
+        config_sections[4]['maintainers'] = u.get_users(3, cursor=cursor)
+        config_sections[4]['admins'] = u.get_users(4, cursor=cursor)
+    config_sections[5]['configured'] = u.configured
+
+    return render_template('config.html', config_sections=config_sections, active_section=u.active_section)
+
+
+@config_app.route('/config/check_connection/<int:target>', methods=('POST',))
+def check_connection(target):
+    session['config/active_section'] = target
+    session['check/'+str(target)] = u.validate_connection_params(target)
+    return redirect(request.referrer, code=307)
+
+
+@config_app.route('/config/save/<int:target>', methods=('POST',))
+def save(target):
+    if u.validate_connection_params(target):
+        data = [(option, target, value) for option, value in u.form_reader(target).items()]
+        with u.db_session() as cursor:
+            cursor.executemany("replace into config(option, section, value) VALUES (?, ?, ?)", data)
+        flash("Settings saved!", 'success')
+        session['config/active_section'] = target + 1
+    else:
+        session['check/'+str(target)] = False
+        session['config/active_section'] = target
+    return redirect(request.referrer, code=307)
+
+@config_app.route('/config/sync/<int:target>', methods=('POST',))
+def sync_database(target):
+    session['config/active_section'] = 3
+    if target == 1:
+        # proxmox_sync()
+        pass
+    elif target == 2:
+        u.ldap_sync()
+
+    return redirect(request.referrer, code=307)
+
+@config_app.route('/config/set_permission', methods=('POST',))
+def set_permission():
+    session['config/active_section'] = 4
+    data = u.form_reader(4)
+    print(data)
+    with u.db_session() as cursror:
+        if session['config/setPermType'] == 2:
+            cursror.execute('select userID from user_table where fullname = ?', (data['userID'],))
+            data['userID'] = u.one_row_fix(cursror.fetchone())
+
+        if data['perm'] == 0:
+            cursror.execute('select count(groupID) from group_content where userID = ?', (data['userID'],))
+            if u.one_row_fix(cursror.fetchone()) > 0:
+                data['perm'] = 1
+
+        cursror.execute('update user_table set userPermission = ? where userID = ?', (data['perm'], data['userID']))
+
+    if data['perm']:
+        # TODO call proxmox to enable account data['userID']
+        pass
+    else:
+        # TODO call proxmox to disable account data['userID']
+        pass
+
+    return redirect(request.referrer, code=307)
+
+@config_app.route('/config/set_permission/<int:handle_type>', methods=('POST',))
+def set_permission_by(handle_type):
+    session['config/active_section'] = 4
+    session['config/setPermType'] = handle_type
+    return redirect(request.referrer, code=307)
+
+@config_app.route('/config/finish', methods=('POST',))
+def confirm_and_lock():
+    if not session.get('permLevel') == 4:
+        flash('Permission Missing', 'error')
+        return redirect(request.referrer)
+
+    data = u.form_reader(5)
+    with u.db_session() as cursor:
+        if not data['SECRET_KEY'] in (None,''):
+            cursor.execute("replace into config(option, section, value) VALUES ('SECRET_KEY', 5, ?)", (data['SECRET_KEY'],))
+            flash('SECRET_KEY updated', 'info')
+        cursor.execute("replace into config(option, value) VALUES ('first-run', ?)", (data['first-run'],))
+
+    flash("Settings saved!", 'success')
+    session['config/active_section'] = 5
+    flash("Restart the web server for settings to take effect!", 'info')
+    request.get_data() # clearing form data just in case
+    return redirect(request.referrer)
